5981a493fbe64c2aed1427280035bd7b8084176f
emmadal <emmanueldalougou@gmail.com>
2022-02-05 18:25:26 +0000

    rename app

--- ios/pharmacy/AppDelegate.m
+++ ios/pharmacy/AppDelegate.m
@@ -36,7 +36,7 @@ static void InitializeFlipper(UIApplication *application) {
 
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
-                                                   moduleName:@"pharmacy"
+                                                   moduleName:@"Pharmacy"
                                             initialProperties:nil];
 
   if (@available(iOS 13.0, *)) {
--- ios/pharmacyTests/pharmacyTests.m
+++ ios/pharmacyTests/pharmacyTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface pharmacyTests : XCTestCase
+@interface PharmacyTests : XCTestCase
 
 @end
 
-@implementation pharmacyTests
+@implementation PharmacyTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
